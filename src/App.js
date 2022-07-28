f17db248a89819bc7ecf65b427a8e13de211fac7
TabithaKoech <tabbyk1718@gmail.com>
2022-07-28 11:58:15 +0300

    Added initial files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React ,{useState}from 'react';
+// import NavBar from './Components/NavBar';
+
+
+function App() {
+  return (
+    <div>
+    cocktail lovers
+    </div>
+  );
+}
+
+export default App;
