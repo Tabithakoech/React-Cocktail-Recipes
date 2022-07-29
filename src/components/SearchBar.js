3fed56eea26c25ed6c995788365726f12a280c53
TabithaKoech <tabbyk1718@gmail.com>
2022-07-29 09:38:54 +0300

    Created a serachform component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,41 @@
+// import React,{useState} from 'react'
+// import CocktailList from './CocktailList';
+
+// function SearchBar() {
+//     const[search,setSearch]=useState("");
+//     const[Mycocktail,setCocktail]=useState();
+//     const searchCocktail=(evt)=>{
+//         if(evt.key=="Enter")
+//         {
+//             fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`)
+//             .then(res=>res.json())
+//             .then(data=> {setCocktail(data.drinks);
+//                 setSearch("")})
+//         }
+//     }
+//   return (
+//     <>
+//       <div className="searchBox">
+//         <input
+//           type="search"
+//           className="search-bar"
+//           onChange={(e) => setSearch(e.target.value)}
+//           value={search}
+//           onKeyPress={searchCocktail}
+//         />
+//       </div>
+//       <div className="container">
+//         {Mycocktail == null ? (
+//           <p className="notSearch">Not found</p>
+//         ) : (
+//           Mycocktail.map((res) => {
+//            <li> key={res.idDrink}</li>
+//             return <CocktailList data={res} />;
+//           })
+//         )}
+//       </div>
+//     </>
+//   );
+// }
+
+// export default SearchBar
