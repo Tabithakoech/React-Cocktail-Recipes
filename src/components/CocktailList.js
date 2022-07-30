fe4734b7202363a4667f76c54189e3915ed0d362
TabithaKoech <tabbyk1718@gmail.com>
2022-07-30 12:39:52 +0300

    created cocktail list component that allows for the data from the api to be passed and added a usestate hook

---
+++ src/components/CocktailList.js
@@ -0,0 +1,41 @@
+import React ,{useState}from 'react'
+// import LinesEllipsis from "react-lines-ellipsis";
+import '../App.css';
+
+
+function CocktailList(getCocktail) {
+
+
+    console.log(getCocktail.data)
+    const[isClicked,setIsClicked]=useState(false)
+
+
+//     return (
+//         <>
+//             <div className="col-md-4">
+//                 <div className="card">
+//                     <img src={getCocktail.data.strDrinkThumb} alt="meal" />
+//                     <div className="info">
+//                         <h2>{getCocktail.data.strDrink}<span><button className='likee' isClicked={isClicked} onClick={()=>
+//                         setIsClicked(!isClicked)
+//                         }>❤️</button></span></h2>
+//                         <p>{getCocktail.data.strIngredient} food</p>
+//                     </div>
+//                     <div className="recipe">
+//                         <h2>Recipe</h2>
+//                         <p>{getCocktail.data.strInstructions} </p>
+//                         {/* <LinesEllipsis
+//                            text={getCocktail.data.strInstructions}
+//                            maxLine="3"
+//                            ellipsis="..."
+//                            trimRight
+//                            basedOn="letters"
+//                        /> */}
+//                     </div>
+//                 </div>
+                
+//             </div>
+//         </>
+//  )
+// }
+// export default CocktailList
