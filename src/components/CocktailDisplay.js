8f155a3a46a3cf9b649aaec908639b2f3be7429d
TabithaKoech <tabbyk1718@gmail.com>
2022-07-30 12:50:38 +0300

    changes to initial files

---
+++ src/components/CocktailDisplay.js
@@ -0,0 +1,26 @@
+// import React,{useEffect, useState} from 'react'
+
+function CocktailDisplay() {
+    // const [cocktailContent,setCocktailContent]=useState([]);
+    // useEffect(()=>{
+    //     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    //     .then(res=>res.json())
+    //     .then(data=>setCocktailContent(data.drinks))
+    //     console.log(cocktailContent)
+    // }
+    // ,[])
+
+
+
+    // fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=')
+    // .then(res=>res.json())
+    // .then(data=> {
+    //     console.log(data)
+    // }
+    // )
+  return (
+    <div>CocktailDisplay</div>
+  )
+}
+
+export default CocktailDisplay
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,7 +5,7 @@ function SearchBar() {
     const[search,setSearch]=useState("");
     const[Mycocktail,setCocktail]=useState();
     const searchCocktail=(evt)=>{
-        if(evt.key=="Enter")
+        if(evt.key==="Enter")
         {
             fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`)//fetching the data from the api
             .then(res=>res.json())
@@ -13,14 +13,17 @@ function SearchBar() {
                 setSearch("")})
         }
     }
-  }
-  
   return (
     <>
+      <div className="heading">
+       <h1>Search Your Cocktail Recipe</h1>
+       <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque tempore unde sed ducimus voluptates illum!</h4>
+      </div>
       <div className="searchBox">
         <input
           type="search"
           className="search-bar"
+          placeholder='Search your  favourite cocktail'
           onChange={(e) => setSearch(e.target.value)}
           value={search}
           onKeyPress={searchCocktail}
@@ -28,7 +31,7 @@ function SearchBar() {
       </div>
       <div className="container">
        {Mycocktail == null ? (
-         <p className="notSearch">Not found</p>
+         <p className="notSearch">No Cocktails Matched your search!</p>
         ) : (
            Mycocktail.map((res) => {
             <li> key={res.idDrink}</li>
@@ -38,5 +41,6 @@ function SearchBar() {
       </div>
      </>
   );
+ }
  
  export default SearchBar
