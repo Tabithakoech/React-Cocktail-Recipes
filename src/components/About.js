bdcc4bee7df6f05c8b199b5a043541ce4dc9d7ca
TabithaKoech <tabbyk1718@gmail.com>
2022-07-30 15:26:34 +0300

    added a description

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <h1 className='about'>This is an intuitive and aesthetic web application for cocktail enthusiasts
+seeking to broaden their horizons and find new, interesting cocktail drinks and their recipes.</h1>
+    </div>
   )
 }
 
