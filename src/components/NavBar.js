1a860a4ee058056ee1dcdeb530f35974e83f460a
TabithaKoech <tabbyk1718@gmail.com>
2022-07-30 12:44:13 +0300

    created the navigation links for the nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,27 @@
-
-
 import React from "react";
-import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
-    <nav className="navBarPosition">
-      <div className="nav-center">
-       
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
 
-        <ul className="navLink">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div className="collaps" id="navt">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <h3 className="logo">Cocktail Bazaar</h3>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/about">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/reviews">Favourites❤️</a>
+        </li>
+      </ul>
+    </div>
+ </div>
+</nav>
   );
 };
 
